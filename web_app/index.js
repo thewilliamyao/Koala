77e88aaf005eb4fbc88e599e1cc60b94e8b60345
wyao7 <wyao7@jhu.edu>
2016-10-23 01:39:18 -0400

    now print translation very jankyly to console, trying to implement socket.io

--- web_app/index.js
+++ web_app/index.js
@@ -2,13 +2,61 @@
 const path = require('path')  
 const express = require('express')  
 const exphbs = require('express-handlebars')
-const app = express()  
+const Translate = require('@google-cloud/translate');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var app = require('express')();
 const port = 3300
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+// Your Translate API key
+const apiKey = 'AIzaSyCP-TFmvNTa7JuoQCNt0q5f8WjzT_IVkIc';
+
+// Instantiates a client
+const translateClient = Translate({
+  key: apiKey
+});
+
+
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+// });
+
 
 app.get('/', (request, response) => {  
   response.render('home', {
     name: 'Mudgett'
   })
+})
+
+app.post('/translate', (request, response) => {
+  var text = request.body.data_message,
+        target = request.body.data_country;
+
+  var local_translation = '';
+  // The text to translate
+  console.log(request.body);
+
+  // const text = ('request.data_message');
+  // // The target language
+  // const target = ('request.data_country');
+
+  // Translates some text into Russian
+  translateClient.translate(text, target, (err, translation) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log(`Text: ${text}`);
+    console.log(`Translation: ${translation}`);
+    local_translation = translation;
+  });
+  return local_translation;
 })
 
 // app.use(express.static('style.css'));
@@ -29,4 +77,7 @@ app.engine('.hbs', exphbs({
 }))
 app.set('view engine', '.hbs')  
 app.set('views', path.join(__dirname, 'views'))  
+
+
+
 
